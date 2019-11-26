714bf1de716831ddf0cafef7283007d9c3a8ce99
Yurii <yuri.sologub@lizard-soft.com>
2019-11-26 14:26:44 +0200

    feat(context): fixed

--- projects/angular-exemplify/src/lib/angular-exemplify.component.ts
+++ projects/angular-exemplify/src/lib/angular-exemplify.component.ts
@@ -4,11 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'ex-angular-exemplify',
   template: `
     <p>
-<<<<<<< HEAD
-      angular-exemplify works! oh we can win the race why not
-=======
       angular-exemplify works! oh we can win the race
->>>>>>> 0dac8299b4ec9988c9d7cc1434be2079707c4703
     </p>
   `,
   styles: []
